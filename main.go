e09464c55b5c87eb0d864a9701a75a321a7bd65d
Jinx-Heniux <jinx.heniux.2019@gmail.com>
2022-10-18 12:22:13 +0200

    p4

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"log"
 	"net/http"
 	"os"
@@ -14,22 +13,6 @@ type IndexData struct {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-
-	// w.Write([]byte("hello jun blog"))
-
-	// jsonS, _ := json.Marshal("hello jun blog")
-	// w.Write(jsonS)
-
-	var indD IndexData
-	indD.Title = "Jun Blog"
-	indD.Desc = "The first Blog"
-	indJson, _ := json.Marshal(indD)
-	w.Write(indJson)
-
-}
-
-func indexHTML(w http.ResponseWriter, r *http.Request) {
 
 	var indD IndexData
 	indD.Title = "Jun Blog"
@@ -52,8 +35,6 @@ func main() {
 	}
 
 	http.HandleFunc("/", index)
-
-	http.HandleFunc("/index.html", indexHTML)
 
 	if err := server.ListenAndServe(); err != nil {
 		log.Println(err)
