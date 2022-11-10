191161c05d75529d35be1c679aa0a99899599aaa
Jinx-Heniux <jinx.heniux.2019@gmail.com>
2022-11-10 13:18:59 +0100

    mysql db and posts

--- service/index.go
+++ service/index.go
@@ -1,6 +1,70 @@
 package service
 
 import (
+	"html/template"
+
+	"github.com/Jinx-Heniux/jun-golang-blog/config"
+	"github.com/Jinx-Heniux/jun-golang-blog/dao"
+	"github.com/Jinx-Heniux/jun-golang-blog/models"
+)
+
+func GetAllIndexInfo(page, pageSize int) (*models.HomeResponse, error) {
+	categorys, err := dao.GetAllCategory()
+	if err != nil {
+		return nil, err
+	}
+	posts, err := dao.GetPostPage(page, pageSize)
+	if err != nil {
+		return nil, err
+	}
+	var postMores []models.PostMore
+	for _, post := range posts {
+		categoryName := dao.GetCategoryNameById(post.CategoryId)
+		userName := dao.GetUserNameById(post.UserId)
+		content := []rune(post.Content)
+		if len(content) > 100 {
+			content = content[0:100]
+		}
+		postMore := models.PostMore{
+			post.Pid,
+			post.Title,
+			post.Slug,
+			template.HTML(content),
+			post.CategoryId,
+			categoryName,
+			post.UserId,
+			userName,
+			post.ViewCount,
+			post.Type,
+			models.DateDay(post.CreateAt),
+			models.DateDay(post.UpdateAt),
+		}
+		postMores = append(postMores, postMore)
+	}
+	//11  10 2  10 1 9 1  21 3
+	//  (11-1)/10 + 1 = 2
+	total := dao.CountGetAllPost()
+	pagesCount := (total-1)/10 + 1
+	var pages []int
+	for i := 0; i < pagesCount; i++ {
+		pages = append(pages, i+1)
+	}
+	var hr = &models.HomeResponse{
+		config.Cfg.Viewer,
+		categorys,
+		postMores,
+		total,
+		page,
+		pages,
+		page != pagesCount,
+	}
+	return hr, nil
+}
+
+/*
+package service
+
+import (
 	"github.com/Jinx-Heniux/jun-golang-blog/config"
 	"github.com/Jinx-Heniux/jun-golang-blog/dao"
 	"github.com/Jinx-Heniux/jun-golang-blog/models"
@@ -36,3 +100,4 @@ func GetAllIndexInfo() (*models.HomeResponse, error) {
 	}
 	return hr, nil
 }
+*/
